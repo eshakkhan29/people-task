6e6bf7d8fe214875497bbb57972737551314c78e
eshakkhan29 <eshakkhan29@gmail.com>
2022-07-23 08:31:09 +0600

    make list

--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InfoList = () => {
+const InfoList = ({ data }) => {
+    const [details, setDetails] = useState(false);
+    const { name, gender, birth_year: year, height, mass, hair_color: hair, skin_color: skin, eye_color: eye, created, edited } = data;
     return (
-        <div>
-
+        <div className="flex justify-around  pt-10 pb-14 items-center ">
+            <p className="text-gray-600 text-sm">{name}</p>
+            <div>
+                <h2>Gender</h2>
+                <p className="text-gray-600 text-sm">{gender}</p>
+            </div>
+            <div>
+                <h2>Birth Year</h2>
+                <p className="text-gray-600 text-sm">{year}</p>
+            </div>
+            <div>
+                <h2>Height</h2>
+                <p className="text-gray-600 text-sm">{height} cm</p>
+            </div>
+            <button
+                onClick={() => setDetails(!details)}
+                className="bg-pink-500 hover:bg-pink-600 text-white px-3 py-2 rounded-md"
+            >
+                Details
+            </button>
         </div>
     );
 };
